ffca6c8c51b2fee9cbcad8e2352539573a9811b1
Daniel Fockler <dfockler13@gmail.com>
2015-08-26 10:43:59 -0700

    initial commit

---
+++ src/guessing_game/core.clj
@@ -0,0 +1,30 @@
+(ns guessing-game.core
+  (:gen-class))
+
+(defn rand-num
+  "Gets a random number between 1 and 100"
+  []
+  (+ (rand-int 100) 1))
+
+(defn check-input
+  [secret input]
+  (cond
+    (< input secret) "Too small!"
+    (> input secret) "Too big!"
+    :else "You win!"))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Guess the number!")
+  (def secret-num (rand-num))
+  (loop []
+    (println "Please input your guess.")
+    (def input (read-string (read-line)))
+    (println (str "You guessed: " input))
+    (def check (check-input secret-num input))
+    (if (not= "You win!" check)
+      (do 
+        (println check)
+        (recur))
+      (println check))))
